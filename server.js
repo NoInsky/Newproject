04963c31d1042334937315781cef93e3dc19f7c9
NoInsky <106363799+NoInsky@users.noreply.github.com>
2022-05-27 17:52:42 +0900

    routing
    서버 js에서 조인 고치고 두개 만들어서 수정하고 readFile해서 넘어가게 해라

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const express = require('express')
 const path = require('path')
-
-// const express = require('express')
-// const loogger = require('morgan')
-// const simple_module = require('./simple_module.js')
+const loogger = require('morgan')
+const fs = require('fs')
+const bodyParser = require('body-parser')
 
 const app = express()
-app.set('port', process.env.PORT || 12010)
-const _path = path.join(__dirname,'./dist')
+const PORT = 12010
+const _path = path.join(__dirname, './dist')
+console.log(_path);
 
+app.use(loogger('tiny'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( {extends:true} ))
 app.use(express.static(_path))
-const PORT = app.get('port')
-app.listen(PORT, () => {
-  console.log(`nodemon Server running at http://127.0.0.1:${PORT}/`);
-  console.log(` http://127.0.0.1:${PORT}/join`);
-  console.log(`http://127.0.0.1:${PORT}/login`);
 
-
+app.get('/join', (req,res) => {
+  fs.readFile('./dist/join.html',function(error,data){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.end(data)
+  })
+})
+app.post('/joinfrm', (req,res) => {
+  const {body: {id, name, pwd}} = req
+  console.log(id, name ,pwd);
 })
 
-// app.use(loogger('tiny'))
 
-// //request(요청)가 들어올 때
-// app.get("/", simple_module.intro)
-// app.get('/users/:userName/book/:bookName',simple_module.handleBook)
-
-// //server가 실행 상태로 전환
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://127.0.0.1:${PORT}/`);
-// }) 
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
+}) 
