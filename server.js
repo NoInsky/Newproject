f893555a6d306ce79ba52fa745f47cba651189ba
NoInsky <106363799+NoInsky@users.noreply.github.com>
2022-05-27 17:31:25 +0900

    routing
    라우팅 하고 밑에 콘솔 찍어서 값 봐야됨 fs.readFile 해서 다시 넘어가야됨

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
 
+// const express = require('express')
+// const loogger = require('morgan')
+// const simple_module = require('./simple_module.js')
+
 const app = express()
 app.set('port', process.env.PORT || 12010)
 const _path = path.join(__dirname,'./dist')
@@ -15,20 +19,13 @@ app.listen(PORT, () => {
 
 })
 
-const express = require('express')
-const app = express()
-const loogger = require('morgan')
-const PORT = 3000
-const simple_module = require('./simple_module.js')
+// app.use(loogger('tiny'))
 
+// //request(요청)가 들어올 때
+// app.get("/", simple_module.intro)
+// app.get('/users/:userName/book/:bookName',simple_module.handleBook)
 
-app.use(loogger('tiny'))
-
-//request(요청)가 들어올 때
-app.get("/", simple_module.intro)
-app.get('/users/:userName/book/:bookName',simple_module.handleBook)
-
-//server가 실행 상태로 전환
-app.listen(PORT, () => {
-  console.log(`Server running at http://127.0.0.1:${PORT}/`);
-}) 
+// //server가 실행 상태로 전환
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://127.0.0.1:${PORT}/`);
+// }) 
