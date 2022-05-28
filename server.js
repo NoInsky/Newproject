ae033403ceb14c5837eafa5e1d3a91d63530bd6b
NoInsky <106363799+NoInsky@users.noreply.github.com>
2022-05-28 17:01:10 +0900

    project

--- server.js
+++ server.js
@@ -21,9 +21,37 @@ app.get('/join', (req,res) => {
   })
 })
 app.post('/joinfrm', (req,res) => {
-  const {body: {id, name, pwd}} = req
-  console.log(id, name ,pwd);
+  const {body: {id, name, pwd, repwd}} = req
+  console.log(name, id, pwd, repwd);
+  fs.readFile('./dist/welcome.html',function(error,data){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.end(data)
 })
+})
+app.post('/welfrm', (req,res) => {
+  const {body: {id, name, pwd, repwd}} = req
+  console.log(name, id, pwd, repwd);
+  fs.readFile('./dist/login.html',function(error,data){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.end(data)
+})
+})
+
+app.get('/login', (req,res) => {
+  fs.readFile('./dist/login.html',function(error,data){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.end(data)
+  })
+})
+app.post('/loginfrm', (req,res) => {
+  const {body: {id, pwd}} = req
+  console.log(id, pwd);
+  fs.readFile('./dist/index.html',function(error,data){
+    res.writeHead(200, {'Content-Type':'text/html'})
+    res.end(data)
+  })
+})
+
 
 
 app.listen(PORT, () => {
